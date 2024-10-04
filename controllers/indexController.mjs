aaf3b014c5467ed788decc137de654d5079737c3
Figby222 <figby222@outlook.com>
2024-10-04 03:39:25 -0700

    Add articlesListGet() ctonroller

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,5 +1,14 @@
+import asyncHandler from "express-async-handler";
+
 function indexRouteGet(req, res) {
     res.render("index", { title: "Node Template" });
 }
 
-export { indexRouteGet };
+const articlesListGet = asyncHandler(async (req, res) => {
+    console.log("Do stuff to get articles");
+    const articles = [];
+
+    res.json(articles);
+})
+
+export { indexRouteGet, articlesListGet };
