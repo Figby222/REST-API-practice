eff27e4fe76dbe13c97fed04d4284b783d4db202
Figby222 <figby222@outlook.com>
2024-10-04 03:34:49 -0700

    Add /articles/:id PUT route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -9,4 +9,6 @@ indexRouter.get("/articles", indexController.articlesListGet);
 
 indexRouter.post("/articles", indexController.createArticlePost)
 
+indexRouter.put("/articles/:id", indexController.updateArticlePut);
+
 export default indexRouter;
